27afeec24e0530205c5d0c135e260f7ed18d0b80
MAjavanta <ma.javanta@gmail.com>
2025-08-05 10:38:19 +0100

    Complete day 3

--- 2024/03/solution.py
+++ 2024/03/solution.py
@@ -7,13 +7,32 @@ def get_product(instruction):
     return int(nums[0]) * int(nums[1])
 
 
+def get_on_instructions(instruction_list):
+    on_instructions = []
+    read_instruction = True
+
+    for instruction in instruction_list:
+        if instruction == "do()":
+            read_instruction = True
+            continue
+        elif instruction == "don't()":
+            read_instruction = False
+            continue
+        elif read_instruction:
+            on_instructions.append(instruction)
+    return on_instructions
+
+
 if __name__ == "__main__":
     good_instructions = []
     sum = 0
     input_file = utils.get_input_file_by_env("03")
     with open(input_file, "r") as f:
         for line in f.readlines():
-            good_instructions += re.findall(r"mul\([0-9]+,[0-9]+\)", line)
-    for instruction in good_instructions:
+            good_instructions += re.findall(
+                r"mul\([0-9]+,[0-9]+\)|don't\(\)|do\(\)", line
+            )
+    on_instructions = get_on_instructions(good_instructions)
+    for instruction in on_instructions:
         sum += get_product(instruction)
     print(sum)
